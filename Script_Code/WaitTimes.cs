9b8b4c6f68e07c6e6c063daef681a73edadf90a7
ericvanbeek <ericvanbeek@foreverdirect.eu>
2018-07-31 07:34:31 +0200

    Created redo logic

---
+++ Script_Code/WaitTimes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using Microsoft.SqlServer.Dts.Runtime;
+using System.Windows.Forms;
+
+namespace ST_c1460709233e45a7af989d19c79a53aa
+{
+	[Microsoft.SqlServer.Dts.Tasks.ScriptTask.SSISScriptTaskEntryPointAttribute]
+	public partial class ScriptMain : Microsoft.SqlServer.Dts.Tasks.ScriptTask.VSTARTScriptObjectModelBase
+	{
+		public void Main()
+		{
+            int waitTimes = Convert.ToInt32(Dts.Variables["$Project::WaitTimes"].Value);
+     
+			System.Threading.Thread.Sleep(waitTimes*1000);
+            Dts.TaskResult = (int)ScriptResults.Success;
+		}
+
+        enum ScriptResults
+        {
+            Success = Microsoft.SqlServer.Dts.Runtime.DTSExecResult.Success,
+            Failure = Microsoft.SqlServer.Dts.Runtime.DTSExecResult.Failure
+        };
+
+
+	}
+}
