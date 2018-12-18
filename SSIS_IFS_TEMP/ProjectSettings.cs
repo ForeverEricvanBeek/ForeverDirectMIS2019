8399536b3fabf79ef605495af5c3170b1a0954fe
Eric van Beek <evanbeek@foreverdirect.eu>
2018-12-18 12:49:04 +0100

    Added archiving for all sources

---
+++ SSIS_IFS_TEMP/ProjectSettings.cs
@@ -0,0 +1,68 @@
+<#
+string SSISDeploymentMode = "Project";
+string stagingProjectName = @"SSIS_IFS_TEMP";
+string DWHProjectName = @"SSIS_IFS_TEMP";
+string projectServerName = @"FDODWHP01";
+string projectWorkDir = @"\\FDODWHP01\StagingFiles";
+string projectLoadingWhere = @"1";
+string CacheDir = projectWorkDir + @"\Biml\SSIS Cache";
+string ImportDir = projectWorkDir + @"\Biml";
+string ImportDirXSD = projectWorkDir + @"\Biml\Input";
+string ExportDir = projectWorkDir + @"\Biml\Output";
+string ArchiveDir = projectWorkDir + @"\Biml\Archive";
+string ErrorDir = projectWorkDir + @"\Biml\Error";
+string projectDatabaseInstanceName = @"DWHIFS"; // Only the instance name, don't include the server name.
+string SystemConnection = "DWI_Control";
+string StagingConnection = "StagingIFS_TEMP";
+
+string projectServerNameAndDatabaseInstanceName = "";
+
+if (projectDatabaseInstanceName.Length == 0) {
+     projectServerNameAndDatabaseInstanceName = projectServerName;
+} else {
+     projectServerNameAndDatabaseInstanceName = projectServerName + @"\" + projectDatabaseInstanceName;
+}
+
+string GeneratorSchemaName = @"generator";
+string conGeneratorConnectionString = @"Data Source=" + projectServerNameAndDatabaseInstanceName + @";Initial Catalog=" + SystemConnection  + ";Provider=SQLNCLI11.1;Integrated Security=SSPI;Auto Translate=False;";
+
+string EffectiveDate = "1900-01-01 00:00:00";
+string ExpiryDate = "9999-12-31 23:59:59";
+string AllCharacterDataTypes = "-nvarchar-varchar-nchar-char-String-AnsiString-StringFixedLength-AnsiStringFixedLength-";
+string AllNumericDataTypes = "-decimal-numeric-float-Decimal-Double-";
+string AllIntegerDataTypes = "-INT    -smallint-int-bigint-Int8-Int16-Int32-Int64-";
+string AllDateDataTypes = "-date-datetime-smalldatetime-Date-DateTime-";
+string AllBooleanDataTypes = "-bit-";
+string ProjectConnectionOption = "CreateInProject=\"true\"";
+string ProjectProtectionLevel = "DontSaveSensitive";
+
+// source, incremental / modified key column seperator
+string[] varKeyColumnSeparator = new string[] { "&&" };
+
+// error and check variables
+string varSourceExtractErrorType = "P";  // R(ows) or P(ercentage)
+int varSourceExtractErrorThreshold = 5;
+string varCheckHeaderFlatFile = "Y"; // Y(es) or N(o)
+string varCheckHeaderProgram = "E:\\\\Check-Header.ps1";
+
+// flatfile options
+string[] varOptionSeparator = new string[] { "&&" };
+string[] varColumnSeparator = new string[] { "=" };
+string varFlatFileAttributesList = "-FlatFileType-ColumnNamesInFirstDataRow-HeaderRowDelimiter-HeaderRowsToSkip-DataRowsToSkip-IsUnicode-RowDelimiter-TextQualifer-";
+// for future use not implemented yet
+string varFlatFileErrorTarget = "database";  // database or flatfile
+
+// lookup
+string[] varSeparatorColumns = new string[] { "&&" };
+string[] varSeparatorColumn = new string[] { "=" };
+
+string varSQLServTableBeginCharacter = "[";
+string varSQLServTableCloseCharacter = "]";
+string varSQLServColumnBeginCharacter = "[";
+string varSQLServColumnCloseCharacter = "]";
+string varEscapeCharacter = "&#92;";
+string varOracleObjectBeginEndCharacter = "&quot;";
+string varODBCObjectBeginEndCharacter = "&quot;";
+
+string varCR = "&#13;";     // carriage return
+#>
