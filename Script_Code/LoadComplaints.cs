1e00b4f07e1c038f2c2601dae360f06f1bdb91a4
Eric van Beek <evanbeek@foreverdirect.eu>
2018-12-27 07:45:38 +0100

    Changed arc files

---
+++ Script_Code/LoadComplaints.cs
@@ -0,0 +1,31 @@
+﻿/*
+using System;
+using System.Data;
+using Microsoft.SqlServer.Dts.Runtime;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ST_f1b41172c66a4ffbabb05ccfbc392398
+{
+	[Microsoft.SqlServer.Dts.Tasks.ScriptTask.SSISScriptTaskEntryPointAttribute]
+	public partial class ScriptMain : Microsoft.SqlServer.Dts.Tasks.ScriptTask.VSTARTScriptObjectModelBase
+    {
+        public void Main()
+		{
+            // TODO: Add your code here
+            string filenpathame = Dts.Variables["User::workingfiletemp"].Value.ToString();
+            string filename = Path.GetFileNameWithoutExtension(filenpathame);
+            Dts.Variables["User::workingfilename"].Value = filename;
+            Dts.TaskResult = (int)ScriptResults.Success;
+
+        }
+
+        enum ScriptResults
+        {
+            Success = Microsoft.SqlServer.Dts.Runtime.DTSExecResult.Success,
+            Failure = Microsoft.SqlServer.Dts.Runtime.DTSExecResult.Failure
+        };
+
+	}
+}
+*/
